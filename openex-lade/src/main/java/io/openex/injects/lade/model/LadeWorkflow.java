aa8a0be9bb458851322d78a939e922c33168ff07
Romuald Lemesle <romuald.lemesle@filigran.io>
2024-01-05 16:19:40 +0100

    [backend] Use lombok

--- openex-lade/src/main/java/io/openex/injects/lade/model/LadeWorkflow.java
+++ openex-lade/src/main/java/io/openex/injects/lade/model/LadeWorkflow.java
@@ -1,50 +1,29 @@
 package io.openex.injects.lade.model;
 
 import io.openex.database.model.ExecutionTrace;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
 public class LadeWorkflow {
 
     private final List<ExecutionTrace> traces = new ArrayList<>();
 
+    @Setter
     private boolean done = false;
 
+    @Setter
     private boolean fail = false;
 
+    @Setter
     private Instant stopTime;
 
     public void addTrace(ExecutionTrace trace) {
         this.traces.add(trace);
     }
 
-    public List<ExecutionTrace> getTraces() {
-        return traces;
-    }
-
-    public Instant getStopTime() {
-        return stopTime;
-    }
-
-    public void setStopTime(Instant stopTime) {
-        this.stopTime = stopTime;
-    }
-
-    public boolean isDone() {
-        return done;
-    }
-
-    public void setDone(boolean done) {
-        this.done = done;
-    }
-
-    public boolean isFail() {
-        return fail;
-    }
-
-    public void setFail(boolean fail) {
-        this.fail = fail;
-    }
 }
