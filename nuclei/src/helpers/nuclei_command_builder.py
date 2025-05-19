3e6a8d617f61cea624aacc2c58c3e1d4a85d4fa4
Sébastien MIGUEL <sebastien.miguel@filigran.io>
2025-05-19 10:34:54 +0200

    Update nuclei_command_builder.py

--- nuclei/src/helpers/nuclei_command_builder.py
+++ nuclei/src/helpers/nuclei_command_builder.py
@@ -1,6 +1,6 @@
 from typing import Dict, List
 
-from nuclei.src.contracts.constants import (
+from contracts.constants import (
     CLOUD_SCAN_CONTRACT,
     CVE_SCAN_CONTRACT,
     EXPOSURE_SCAN_CONTRACT,
